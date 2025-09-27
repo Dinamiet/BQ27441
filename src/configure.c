1a8a7387dcccf1fdfe25128c46e296600de0de6f
Dinamiet <iceman.sm39@gmail.com>
2025-09-27 17:48:47 +0200

    Correct endianness on data written

--- src/configure.c
+++ src/configure.c
@@ -1,21 +1,26 @@
 #include "bq27441.h"
 #include "commands.h"
+#include "endianness.h"
 #include "extended.h"
 
 bool BQ27441_Configure(BQ27441* bq, uint16_t capacity, uint16_t terminateVoltage, uint16_t taperCurrent)
 {
-	if (!extended_Write(bq, EXTENDED_STATE_CLASSID, EXTENDED_STATE_DESIGN_CAPACITY_OFFSET, &capacity, sizeof(capacity)))
+	uint16_t energy    = capacity * 3.7f;
+	uint16_t taperRate = 10 * capacity / taperCurrent;
+
+	uint16_t writeValue = BIG_ENDIAN_16(capacity);
+	if (!extended_Write(bq, EXTENDED_STATE_CLASSID, EXTENDED_STATE_DESIGN_CAPACITY_OFFSET, &writeValue, sizeof(writeValue)))
 		return false;
 
-	uint16_t energy = capacity * 3.7f;
-	if (!extended_Write(bq, EXTENDED_STATE_CLASSID, EXTENDED_STATE_DESIGN_ENERGY_OFFSET, &energy, sizeof(energy)))
+	writeValue = BIG_ENDIAN_16(energy);
+	if (!extended_Write(bq, EXTENDED_STATE_CLASSID, EXTENDED_STATE_DESIGN_ENERGY_OFFSET, &writeValue, sizeof(writeValue)))
 		return false;
 
 	if (!extended_Write(bq, EXTENDED_STATE_CLASSID, EXTENDED_STATE_TERMINATE_VOLTAGE_OFFSET, &terminateVoltage, sizeof(terminateVoltage)))
 		return false;
 
-	uint16_t taperRate = 10 * capacity / taperCurrent;
-	if (!extended_Write(bq, EXTENDED_STATE_CLASSID, EXTENDED_STATE_TAPER_RATE_OFFSET, &taperRate, sizeof(taperRate)))
+	writeValue = BIG_ENDIAN_16(taperRate);
+	if (!extended_Write(bq, EXTENDED_STATE_CLASSID, EXTENDED_STATE_TAPER_RATE_OFFSET, &writeValue, sizeof(writeValue)))
 		return false;
 
 	return true;
